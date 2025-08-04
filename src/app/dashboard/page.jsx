43c6bd7d460cf9d2469de33c21f0562972199d80
Chris Diorio <chris.diorio12@gmail.com>
2025-08-03 22:18:42 -0500

    Add basic dashboard

---
+++ src/app/dashboard/page.jsx
@@ -0,0 +1,23 @@
+import { createClient } from '../lib/supabase/server';
+import { redirect } from 'next/navigation';
+import SignOutButton from '../components/SignOutButton'; // We will create this next
+
+export default async function DashboardPage() {
+  const supabase = createClient();
+
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
+
+  if (!user) {
+    redirect('/login');
+  }
+
+  return (
+    <div>
+      <h1>Hello, {user.email}</h1>
+      <p>This is your protected dashboard.</p>
+      <SignOutButton />
+    </div>
+  );
+}
