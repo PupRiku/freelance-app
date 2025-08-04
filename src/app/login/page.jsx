0c5df737a7665b3ffdf09d26cd8b8367cd74fd96
Chris Diorio <chris.diorio12@gmail.com>
2025-08-03 22:10:14 -0500

    Simple login

---
+++ src/app/login/page.jsx
@@ -0,0 +1,64 @@
+'use client';
+
+import { useState } from 'react';
+import { useRouter } from 'next/navigation';
+import { createClient } from '../lib/supabase/client';
+
+export default function LoginPage() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const router = useRouter();
+  const supabase = createClient();
+
+  const handleSignIn = async (e) => {
+    e.preventDefault();
+    const { error } = await supabase.auth.signInWithPassword({
+      email,
+      password,
+    });
+
+    if (error) {
+      return alert('Error: Could not authenticate user');
+    }
+
+    router.push('/');
+    router.refresh();
+  };
+
+  const handleSignUp = async (e) => {
+    e.preventDefault();
+    await supabase.auth.signUp({
+      email,
+      password,
+      options: {
+        emailRedirectTo: `${location.origin}/auth/callback`,
+      },
+    });
+
+    alert('Check your email to confirm sign up!');
+    router.refresh();
+  };
+
+  return (
+    <div>
+      <h2>Sign In / Sign Up</h2>
+      <form>
+        <label htmlFor="email">Email</label>
+        <input
+          name="email"
+          onChange={(e) => setEmail(e.target.value)}
+          value={email}
+        />
+        <label htmlFor="password">Password</label>
+        <input
+          type="password"
+          name="password"
+          onChange={(e) => setPassword(e.target.value)}
+          value={password}
+        />
+        <button onClick={handleSignIn}>Sign In</button>
+        <button onClick={handleSignUp}>Sign Up</button>
+      </form>
+    </div>
+  );
+}
